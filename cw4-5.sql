683b8c409264004d0886be9c712033173c6919d0
ragnathurdx <47988830+ragnathurdx@users.noreply.github.com>
2021-01-29 20:57:42 +0100

    Create cw4-5.sql

---
+++ cw4-5.sql
@@ -0,0 +1,16 @@
+/* 5.Za pomocą tabeli INDEX BY wyciągnąć informacje
+o nazwach departamentów i wypisać na ekran 10 (numery 10,20,…,100) */
+
+DECLARE
+    TYPE temp IS TABLE OF departments.department_name%TYPE INDEX BY pls_integer;
+    departments__list temp_table;
+    id_numbers NUMBER:=10;
+BEGIN
+    FOR i IN 1..10
+        LOOP
+        SELECT department_name INTO departments__list(i) FROM departments 
+        WHERE department_id = id_numbers;
+        id_numbers := id_numbers + 10;
+        dbms_output.put_line(departments__list(i));
+        END LOOP;
+END;
